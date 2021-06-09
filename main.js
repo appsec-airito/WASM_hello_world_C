18f90f3f2e6ca5050daad2b96ac94f980cf60130
Airito <aswhinpaulantony@gmail.com>
2021-06-09 06:09:36 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+(async () => {
+    const codePromise = fetch('../out/main.wasm')
+    const {
+        instance
+    } = await WebAssembly.instantiateStreaming(codePromise)
+    console.log(instance)
+
+    //access the contents of an exported memory via buffer
+    const buffer = new Uint8Array(instance.exports.memory.buffer) //contain an array buffer something like [ 12, 23445, 34, 1, 'H', 'e', 'l', 'l', 'o',.......]
+
+    //getting the pointer where the return for the helloWorld() starts and iterating through the buffer
+    const pointer = instance.exports.helloWorld()
+    let string1 = ""
+    for (let i = pointer; buffer[i]; i++) {
+        string1 += String.fromCharCode(buffer[i])
+    }
+    //passing the string the console
+    console.log(string1)
+
+    //passing the string to Document
+    document.getElementById("container").textContent = string1;
+
+})()
